3395f3be7e1b0d4932e84635c1d235e573208bd3
LakkinziMusic <lakkinzimusic@gmail.com>
2022-03-15 14:54:43 +0300

    Seed

--- database/seeding/20220313191407_register_queries.up.sql
+++ database/seeding/20220313191407_register_queries.up.sql
@@ -1,2 +1,2 @@
-insert into public.register_queries (id, name, type, register_id)
-values  ('9e3b05b1-3007-4cc1-9d65-58d3211e7153', 'Выгрузить данные (в разработке)', 'plain', 'a0680b34-0d9b-4df2-9288-fbc421fd3ee4');
+insert into public.register_queries (id, name, type, register_id, key)
+values  ('9e3b05b1-3007-4cc1-9d65-58d3211e7153', 'Выгрузить данные (в разработке)', 'plain', 'a0680b34-0d9b-4df2-9288-fbc421fd3ee4', 'ФИО');
