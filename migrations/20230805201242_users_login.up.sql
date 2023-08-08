d86e4ca78210653f526876dd6277af52fa669c88
LakkinziMusic <lakkinzimusic@gmail.com>
2023-08-08 10:04:28 +0300

    UserLogin

---
+++ migrations/20230805201242_users_login.up.sql
@@ -0,0 +1,9 @@
+UPDATE users SET login = 'Ковтун Е.И' WHERE id = 'bc3bf514-834c-416b-a2e6-a349f4b4ecce';
+UPDATE users SET login = 'АдминАнафилаксия' WHERE id = '7ef578b3-86c5-421f-8f6a-2a4de2cbf6dd';
+UPDATE users SET login = 'Иванов А.А' WHERE id = '86f5fb02-f65c-431b-a7fd-849369d9c21b';
+UPDATE users SET login = 'Орлова.j.О' WHERE id = '1c05d56c-5b98-4361-b42c-ebd5107cfb5e';
+UPDATE users SET login = 'Больница№52' WHERE id = '59f39975-83ab-434d-954c-8b6fd1ffa4a2';
+UPDATE users SET login = 'admin2' WHERE id = '66f6fe6b-4cbf-4bfa-96de-cdaadb4febbd';
+UPDATE users SET login = 'Зимин С.Б' WHERE id = '342221bf-810d-4909-b68a-da939783fba1';
+UPDATE users SET login = 'admin3' WHERE id = '5d04ac28-70d0-4926-adca-5da55a6cf7e3';
+UPDATE users SET login = 'admin1' WHERE id = '5d04ac28-70d0-4926-adca-5da55a6cf7e4';
