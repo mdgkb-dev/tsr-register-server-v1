8ab3194de3b81b20b85af5073b6127127ff11280
LakkinziMusic <lakkinzimusic@gmail.com>
2023-09-05 16:50:58 +0300

    NestedResearch

---
+++ migrations/20230905135001_add_question.up.sql
@@ -0,0 +1,2 @@
+insert into public.questions (id, name, item_order, value_type_id, with_other, research_id, short_name, age_compare, code, calculate_scores, parent_id, comment)
+values  ('bbd82f60-1f80-4cda-822a-a5c18bc2e75c', 'Уточнить', null, '9f61f302-6821-40b9-94bc-78dedf955a11', false, null, null, null, null, null, 'fc171666-349d-4c79-86fc-cb6a2bfd2baf', null);
