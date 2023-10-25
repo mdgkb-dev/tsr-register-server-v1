7c9a2bd39a3d53a12493249a8ef20b44a8690ef1
Syrtsev <sirtsevilya@yandex.ru>
2023-10-25 23:24:49 +0300

    Added drug search group

---
+++ migrations/20231025121225_add_search_group_seed.up.sql
@@ -0,0 +1,26 @@
+INSERT INTO
+    public.search_groups (
+        id,
+        key,
+        label,
+        search_group_order,
+        route,
+        search_group_table,
+        search_column,
+        label_column,
+        value_column,
+        description_column
+    )
+VALUES
+    (
+        'b1fbcff6-8aac-4a86-830e-ba3579c7f910',
+        'drug',
+        'name_inn',
+        NULL,
+        NULL,
+        'drugs',
+        'name_inn',
+        'name_inn',
+        'id',
+        NULL
+    );
