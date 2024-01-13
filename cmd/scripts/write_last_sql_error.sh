110792720f618df8387ca4d803c44534c7abde7a
DmitriyKosterin <dancrimstudio@gmail.com>
2024-01-13 14:46:45 +0300

    d

--- cmd/scripts/write_last_sql_error.sh
+++ cmd/scripts/write_last_sql_error.sh
@@ -5,4 +5,4 @@ last_error_file=./logs/errors/_last_error.sql
 
 tail -c+1 $act_log_file | grep -E 'SQLSTATE' | grep -E 'msg' | awk -F: '{print $3}' | grep -v ERROR | tail -n 1 >$last_error_file
 
-cat $last_error_file
+cat $last_error_file 
